d92e327b4886ff733e428b0afca951afb70912f9
Tobias Wesper <wesper@in.tum.de>
2014-02-18 12:33:47 +0100

    Added MaskingOperatorRandomShiftDecimal.

--- src/main/org/deidentifier/arx/masking/MaskingOperatorRandomShiftDate.java
+++ src/main/org/deidentifier/arx/masking/MaskingOperatorRandomShiftDate.java
@@ -1,6 +1,9 @@
 package org.deidentifier.arx.masking;
 
 import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Random;
 
 /**
  * Example for a non-generic masking operator on instance level
@@ -9,6 +12,10 @@ public class MaskingOperatorRandomShiftDate extends AbstractInstanceMaskingDate{
 
 	@Override
 	protected Date maskInternal(Date input) {
+		
+		Calendar cal = new GregorianCalendar();
+		cal.setTime(input);
+		// TODO complete
 		
 		/*
 		 * RETURN RANDOM SHIFTED DATE
---
+++ src/main/org/deidentifier/arx/masking/MaskingOperatorRandomShiftDecimal.java
@@ -0,0 +1,17 @@
+package org.deidentifier.arx.masking;
+
+import java.util.Random;
+
+public class MaskingOperatorRandomShiftDecimal extends AbstractInstanceMaskingDecimal {
+
+	@Override
+	protected double maskInternal(double input) {
+		
+		/*
+		 * Return random shifted decimal
+		 */
+		
+		return 0;
+	}
+
+}
