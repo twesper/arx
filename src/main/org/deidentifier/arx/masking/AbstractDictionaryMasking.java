0777bdbbaa35f9bc98a8d804af59635d5129a45a
Tobias Wesper <wesper@in.tum.de>
2014-03-02 23:42:10 +0100

    More work on masking classes; added second template parameter...

--- src/main/org/deidentifier/arx/masking/AbstractDictionaryMasking.java
+++ src/main/org/deidentifier/arx/masking/AbstractDictionaryMasking.java
@@ -1,14 +1,6 @@
 package org.deidentifier.arx.masking;
 
-import org.deidentifier.arx.DataType;
-
-/**
- * TEST
- * @author Tobi
- *
- * @param <T>
- */
-public abstract class AbstractDictionaryMasking<T extends DataType<?>> {
+public abstract class AbstractDictionaryMasking {
 	
 	public abstract String[] mask(String[] input);
 }
--- src/main/org/deidentifier/arx/masking/AbstractInstanceMasking.java
+++ src/main/org/deidentifier/arx/masking/AbstractInstanceMasking.java
@@ -2,7 +2,20 @@ package org.deidentifier.arx.masking;
 
 import org.deidentifier.arx.DataType;
 
-public abstract class AbstractInstanceMasking<T extends DataType<?>> {
-
-	public abstract String mask(String input);
+/**
+ * 
+ * @author Tobi
+ *
+ * @param <T>
+ * @param <S>
+ * Requires both parameters because of type erasure
+ */
+public abstract class AbstractInstanceMasking<T extends DataType<S>, S> {
+	
+	public String mask(String input) {
+		
+		
+	}
+	
+	public abstract S maskInternal(S input);
 }
